82c584e40c78ae2e50dba3aa14736812ce3b708c
Joyce Liu <jambajoyce@gmail.com>
2014-11-26 21:52:43 -0500

    picture + link

--- app.js
+++ app.js
@@ -2,13 +2,21 @@ var APP_KEY = '22951439b596a4f6e848fe61afacbb94';
 var APP_ID = '6ba4073d';
 var myID = "_app_id=" + APP_ID + "&_app_key=" + APP_KEY;
 
-var searchURL = "http://api.yummly.com/v1/api/recipes?_app_id=" + APP_ID + "&_app_key=" + APP_KEY + "&maxResult=100&start=0" + "&requirePictures=true&" 
+var xmlhttp = new XMLHttpRequest();
+
+// var searchURL = "http://api.yummly.com/v1/api/recipes?_app_id=" + APP_ID + "&_app_key=" + APP_KEY + "&maxResult=100&start=0" + "&requirePictures=true&" 
+var searchURL = "http://api.yummly.com/v1/api/recipes?" + myID + "&maxResult=100&start=0" + "&requirePictures=true&" 
 var dinner = "q=dinner"
 var chicken = "q=chicken"
 var URL = "http://api.yummly.com/v1/api/recipe/recipe-id?" + myID;
 
-var xmlhttp = new XMLHttpRequest();
+var recipeURL = "http://api.yummly.com/v1/api/recipe/recipe-id?_app_id=" + APP_ID + " &_app_key=" + APP_KEY + ""
+
+
+
+// each object in the array contains the name of the recipe, image url of the recipe, and link to the recipe
 var recipesArray = [];
+var counter = 0;
 
 function main() {
     $('.icon-menu').click( function() {
@@ -48,15 +56,59 @@ function httpGet(theUrl) {
 
         var imgURL = obj.matches[i].smallImageUrls[0];
         imgURL = imgURL.substring(0, imgURL.length - 4);
-
-
         recipesArray[i].pic = imgURL;
-        console.log(recipesArray[i].pic);
+
+        var id = obj.matches[i].id;
+        recipesArray[i].url = "http://www.yummly.com/recipe/" + id;
+
+        console.log(recipesArray[i]);
 
     }
-    console.log(obj.matches);
+// put first recipe onto page
+//    console.log(obj.matches);
+
+    var recipePic = new Image();
+    recipePic.src = recipesArray[counter].pic;
+    console.log("in");
+    console.log(recipePic.src);
+
+    recipePic.onload = function() {
+        //imgTagContents = "<img id='theImg' " + "src=' " + recipePic.src + "'/>";
+        var currentImage = document.getElementById("myImg");
+        currentImage.src = recipePic.src;
+        console.log("onload");
+        //$('.box').prepend(recipePic);
+    };
+
+    $("#rec-link").attr("href", recipesArray[counter].url);
+    $("#rec-link").text(recipesArray[counter].name);
+
+    counter += 1;
 }
 
+function switchRecipe() {
+    //Change picture
+    var recipePic = new Image();
+    recipePic.src = recipesArray[counter].pic;
+    console.log("in");
+    console.log(recipePic.src);
+
+    recipePic.onload = function() {
+        //imgTagContents = "<img id='theImg' " + "src=' " + recipePic.src + "'/>";
+        var currentImage = document.getElementById("myImg");
+        currentImage.src = recipePic.src;
+        console.log("onload");
+        //$('.box').prepend(recipePic);
+    };
+
+    // Change name + link:
+    $("#rec-link").attr("href", recipesArray[counter].url);
+    $("#rec-link").text(recipesArray[counter].name);
+
+
+    counter += 1;
+    console.log("next");
+}
 
 
 //run the main function once web page has fully loaded
